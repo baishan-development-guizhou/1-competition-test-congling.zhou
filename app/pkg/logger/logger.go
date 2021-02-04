f2ac6ab36d6b467feb6eaed413a1b6781ae4c265
clore <1610746939@qq.com>
2021-02-04 18:28:03 +0800

    change：调整log信息

--- app/pkg/logger/logger.go
+++ app/pkg/logger/logger.go
@@ -19,7 +19,7 @@ func init() {
 		log.Fatalln("Faild to open error logger file:", err)
 	}
 	//自定义日志格式
-	Info = log.New(io.MultiWriter(file, os.Stderr), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(io.MultiWriter(file, os.Stderr), "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-	Warning = log.New(io.MultiWriter(file, os.Stderr), "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Info = log.New(io.MultiWriter(file, os.Stderr), "【INFO】: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Error = log.New(io.MultiWriter(file, os.Stderr), "【ERROR】: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Warning = log.New(io.MultiWriter(file, os.Stderr), "【WARNING】: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
